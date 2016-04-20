aad683223e501093dafc2056a1ad52c70419b4fc
camilov <cagudelovillegas@gmail.com>
2016-04-20 13:52:39 -0500

    Quitando las funciones requeridas en el formato 3
    Dejando a un lado las que conllevan usar el icc

--- ALU.vhd
+++ ALU.vhd
@@ -33,11 +33,11 @@ use std.textio.all;
 --use UNISIM.VComponents.all;
 
 entity ALU is
-    Port ( Oprs1 : in  STD_LOGIC;
-           Op2 : in  STD_LOGIC;
-           AluoP : in  STD_LOGIC;
+    Port ( Oprs1 : in  STD_LOGIC_VECTOR(31 downto 0);
+           Op2 : in  STD_LOGIC_VECTOR(31 downto 0);
+           AluoP : in  STD_LOGIC_VECTOR(5 downto 0);
            Rst : in  STD_LOGIC;
-           Result : out  STD_LOGIC);
+           Result : out  STD_LOGIC_VECTOR(31 downto 0));
 end ALU;
 
 
@@ -45,58 +45,29 @@ architecture Behavioral of ALU is
 
 begin
 
-	process(Oprs1, Op2, AluoP)
+process(Oprs1, Op2, AluoP)
 		begin
 			if(Rst = '0')then
 			case(AluoP) is
 				when "000001" => --add
-					Result <= Oprs1 + Op2;
-				when "001001" => --addcc
-					Result <= Oprs1 + Op2;
+					Result <= (Oprs1 + Op2);
 				when "000010" => --sub
 					Result <= Oprs1 - Op2;
-				when "001100" => --subcc
-					Result <= Oprs1 - Op2;
-				when "000011" => --and
+				when "000011" => --And
 					Result <= Oprs1 and Op2;
-				when "001111" => --andcc
-					Result <= Oprs1 and Op2;
-				when "000100" => --or
+				when "000100" => --Or
 					Result <= Oprs1 or Op2;
-				when "010001" => --orcc
-					Result <= Oprs1 or Op2;
-				when "000101" => --nand
+				when "000101" => --Nand
 					Result <= not(Oprs1 and Op2);
-				when "010000" => --nandcc
-					Result <= not(Oprs1 and Op2);
-				when "000111" => -- xor 
+				when "000110" => --Nor
+					Result <= not(Oprs1 or Op2);
+				when "000111" => -- Xor
 					Result <= Oprs1 xor Op2;
-				when "010011" => -- xorcc
-					Result <= Oprs1 xor Op2;
-				when "001000" => -- xnor 
-					Result <= not(Oprs1 xor Op2);
-				when "010100" => -- xnorcc
+				when "001000" => --Xnor
 					Result <= not(Oprs1 xor Op2);
 				
-				when "001010" => -- addx 
-				Result <= Oprs1 + Op2;
-				when "001011" => -- addxcc
-				Result <= Oprs1 + Op2;
-				
-				when "001101" => -- subx 
-				Result <= Oprs1 - Op2;
-				when "001110" => -- subxcc
-				Result <= Oprs1 - Op2;
-				
-				when "010101" => -- SLL
-				Result <= to_stdlogicvector(to_bitvector(Oprs1) sll conv_integer(Op2));
-				when "010110" => -- SRL
-				Result <= to_stdlogicvector(to_bitvector(Oprs1) srl conv_integer(Op2));
-				when "010111" => -- SRA
-				Result <= to_stdlogicvector(to_bitvector(Oprs1) sra conv_integer(Op2));
-		
-				
-				when others => -- Cae el nop
+								
+				when others => 
 				Result <= (others=>'0');
 		end case;
 		
